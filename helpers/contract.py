9f57e235bd95e838b4ba63e86743846e93119526
cpondoc <chris.pondoc@comcast.net>
2021-06-24 13:54:29 -0400

    Begin adding helper functions

---
+++ helpers/contract.py
@@ -0,0 +1,14 @@
+"""
+File: contract.py
+Helper function to create a contract.py 
+"""
+
+from web3 import Web3
+import json
+
+def get_contract(abi_path, address):
+   f = open(abi_path, 'r')
+   abi = json.load(f)
+   web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/f5470eb326af43adadbb81276c2e4675'))
+   f.close()
+   return web3.eth.contract(address=(address), abi=abi)
---
+++ helpers/proper_date.py
@@ -0,0 +1,6 @@
+"""
+File: proper_date.py
+Playing around with getting the proper date
+"""
+
+sample_time = 1624554484
