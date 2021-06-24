97674a3bc2bd184fe36e246e5c7a2b64e3b24975
cpondoc <chris.pondoc@comcast.net>
2021-06-24 13:40:40 -0400

    Added Chainlink ABI

--- scripts/chainlink.py
+++ scripts/chainlink.py
@@ -18,7 +18,8 @@ This function gets the value of all Chainlink Data necessary for running stuff
 """
 def get_chainlink_data(name, address):
     web3 = Web3(Web3.HTTPProvider('https://kovan.infura.io/v3/50b206f08a5745818266c90ac93c86b2'))
-    abi = '[{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
+    f = open('contracts/chainlink.json', 'r')
+    abi = json.load(f)
     contract = web3.eth.contract(address=address, abi=abi)
     num_decimals = contract.functions.decimals().call()
     latestData = contract.functions.latestRoundData().call()
