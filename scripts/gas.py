6d57f41addc1c11e6a19fb3a946db9a4453de97b
cpondoc <chris.pondoc@comcast.net>
2021-07-16 17:19:23 -0400

    Fix Gas Prices code

--- scripts/gas.py
+++ scripts/gas.py
@@ -1,42 +1,40 @@
-from web3 import Web3
-import json
-from datetime import datetime
+'''
+File: gas.py
+Get historical gas prices using Chainlink oracle
 
-""" Constants """
+Notes:
+- Round Data format = [roundId, answer, startedAt, updatedAt, answeredInRound, decimals]
+'''
+
+''' Libraries '''
+from datetime import datetime
+import scripts.helpers.contract
+
+''' Constants '''
 TIME_CHANGE = 20 
 
-"""
-Function: get_contract()
-Get the contract for a specific exchange for Chainlink
-"""
-def get_contract(address):
-    web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/1cc3d60f38f64e469c4ea250ddeb0c54'))
-    f = open('contracts/chainlink_gas.json', 'r')
-    abi = json.load(f)
-    return web3.eth.contract(address=address, abi=abi)
+''' Smart Contract Set-Up'''
+gas_contract = scripts.helpers.contract.get_contract('contracts/chainlink_gas.json', '0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C')
 
-"""
-Function: get_timestamps()
-Get the values and the timestamps for each value
-"""
 def get_timestamps():
+    '''
+    Get the values and the timestamps for each value
+    '''
     timestamps = []
     gas_prices = []
-    contract = get_contract('0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C')
-    data = (contract.functions.latestRoundData().call())
+    data = gas_contract.functions.latestRoundData().call()
     update_id = data[0]
     for i in range(0, TIME_CHANGE):
-        data = contract.functions.getRoundData(update_id).call()
+        data = gas_contract.functions.getRoundData(update_id).call()
         timestamps.append(data[3])
         gas_prices.append(data[1])
         update_id -= 1
     return gas_prices, timestamps
 
-"""
-Function: get_corresponding_indices()
-Gets the corresponding indices of gas prices to time differences
-"""
 def get_corresponding_prices(timestamps, gas_times, gas_prices):
+    '''
+    Gets the corresponding indices of gas prices to time differences
+    '''
     indices = []
     corresponding_prices = []
     for timestamp in timestamps:
